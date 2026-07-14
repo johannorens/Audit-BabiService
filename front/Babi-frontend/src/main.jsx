5d52df29b4e8660a6a7bc0ba370a9e84df496eef
johannorens <johannorens009@gmail.com>
2026-07-14 11:08:53 +0000

    feat: intégration Sentry frontend

--- front/Babi-frontend/src/main.jsx
+++ front/Babi-frontend/src/main.jsx
@@ -1,13 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById('root')).render(
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  environment: "production",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 0.2,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
